5b788e1877c4e562a6edb5f3dd9975535687e557
nanana37 <hrk@macos>
2024-03-22 00:06:26 +0900

    Update install.sh
    Remove bash configs
    Add .zshrc & .tmux.conf

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # Define variables
 DOTFILES="$HOME/dotfiles"
-FILES=".bashrc .bash_profile .inputrc .gitconfig .config/*"
+FILES=".bashrc .bash_profile .inputrc .gitconfig .config/* .zshrc .tmux.conf"
 
-# Set default shell to Bash
-if [ "$(basename "$SHELL")" != "bash" ]; then
-	chsh -s /bin/bash
-fi
+# # Set default shell to Bash
+# if [ "$(basename "$SHELL")" != "bash" ]; then
+# 	chsh -s /bin/bash
+# fi
 # Enable nullglob shell option
-shopt -s nullglob
+# shopt -s nullglob
 
 # Create symbolic links to new dotfiles
 echo "Creating symbolic links..."
@@ -17,5 +17,9 @@ for file in $FILES; do
 	ln -sfn "$DOTFILES/$file" "$HOME/${file##*/}" # Use parameter expansion to get basename
 	echo "Created symlink for: $file"
 done
+
+# Install dependencies
+echo "Installing dependencies..."
+brew install zsh tmux
 
 echo "Installation complete."

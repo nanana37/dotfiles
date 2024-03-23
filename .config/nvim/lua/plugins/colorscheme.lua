8f4af36651a3f69b0150378fa01a3d732c0dbd44
nanana37 <3na7nanana@gmail.com>
2024-03-24 02:08:41 +0900

    Set colorscheme on lazy.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,34 +2,12 @@ return {
   -- Solraized Osaka
   {
     "craftzdog/solarized-osaka.nvim",
-    lazy = false,
+    lazy = true,
     priority = 1000,
     opts = function()
       return {
         transparent = true,
       }
     end,
-  },
-  -- Make Tokyonight transparent
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load color
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "solarized-osaka",
-    },
   },
 }

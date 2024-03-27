04b02069e0b6b7b80f9e80ba243d26f02290282e
nanana37 <3na7nanana@gmail.com>
2024-03-27 21:38:04 +0900

    Change colorscheme to solarized-osaka (transparent!)

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priotiry = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}

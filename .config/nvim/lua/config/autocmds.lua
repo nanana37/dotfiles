4b570599e19cfbb2875d5b5af2f50a04ecafe8bf
nanana37 <3na7nanana@gmail.com>
2024-04-04 22:00:03 +0900

    [Disabled] Add autocmds for disable autoformat

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- -- Disable autoformat for certain filetypes
+-- -- * <Leader>uf workds as well
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "lua" },
+--   callback = function()
+--     vim.b.autoformat = false
+--   end,
+-- })

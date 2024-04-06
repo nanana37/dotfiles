3eff3f062fb771464077c8df54d976899a5be0ea
nanana37 <hrk@macos>
2024-04-06 21:23:11 +0900

    Keys for copilot

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,4 +8,42 @@ return {
       end,
     },
   },
+  -- Copilot
+  {
+    "zbirenbaum/copilot.lua",
+    keys = {
+      {
+        "<leader>cps",
+        require("copilot.command").status,
+        mode = "n",
+        desc = "Copilot Status",
+      },
+      {
+        "<leader>cpt",
+        function()
+          local command = require("copilot.command")
+          local client = require("copilot.client")
+          if client.is_disabled() then
+            command.enable()
+            command.status()
+          else
+            command.disable()
+            command.status()
+          end
+        end,
+        mode = "n",
+        desc = "Toggle Copilot",
+      },
+    },
+    opts = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>cp"] = {
+          name = "Copilot",
+          s = { "Status" },
+          t = { "Toggle" },
+        },
+      })
+    end,
+  },
 }

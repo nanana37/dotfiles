2d48caa500a020a4a50333e8b33677a2880ce60a
nanana37 <hrk@macos>
2024-04-10 06:21:30 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -3,4 +3,8 @@
 brew update
 brew upgrade
 
-brew install bash-git-prompt
+brew install zsh tmux
+brew install bat eza fzf ripgrep powerlevel10k
+
+# This is for bash, and a little slow for me.
+# brew install bash-git-prompt

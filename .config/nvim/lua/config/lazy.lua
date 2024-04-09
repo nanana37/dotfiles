b0bd14e697247e3965e539c4672f90b75d1d3f7e
nanana37 <hrk@macos>
2024-04-10 05:07:56 +0900

    nvim is now catpuccin!

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "solarized-osaka",
+        colorscheme = "catppuccin",
       },
     },
     -- import any extras modules here

8c087197eaeefbb56b6d449b8109131828f49ced
nanana37 <3na7nanana@gmail.com>
2024-04-14 03:47:12 +0900

    Ref: One file for one plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,47 +10,4 @@ return {
       },
     },
   },
-  -- Copilot
-  {
-    "zbirenbaum/copilot.lua",
-    keys = {
-      {
-        "<leader>cps",
-        require("copilot.command").status,
-        mode = "n",
-        desc = "Copilot Status",
-      },
-      {
-        "<leader>cpe",
-        function()
-          local command = require("copilot.command")
-          command.enable()
-          command.status()
-        end,
-        mode = "n",
-        desc = "Enable Copilot",
-      },
-      {
-        "<leader>cpd",
-        function()
-          local command = require("copilot.command")
-          command.disable()
-          command.status()
-        end,
-        mode = "n",
-        desc = "Disable Copilot",
-      },
-    },
-    opts = function()
-      local wk = require("which-key")
-      wk.register({
-        ["<leader>cp"] = {
-          name = "Copilot",
-          s = { "Copilot Status" },
-          e = { "Enable Copilot" },
-          d = { "Disable Copilot" },
-        },
-      })
-    end,
-  },
 }

3350b9607c52cb0ff6efaee5aee7931043610952
nanana37 <hrk@macos>
2024-04-17 19:40:47 +0900

    nvim: plugins are now categorized.
    Increasing the number of files with each additional plugin seems bad for
    me.

--- .config/nvim/lua/plugins/flash.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "folke/flash.nvim",
-  enabled = true,
-}
---
+++ .config/nvim/lua/plugins/hapoon.lua
@@ -0,0 +1,8 @@
+if true then
+  return {}
+end
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+}

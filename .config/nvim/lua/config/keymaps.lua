b582bba9c49c7307e827d111c70c0a410e8873e2
nanana37 <3na7nanana@gmail.com>
2024-04-24 20:13:09 +0900

    Update config of leetcode.nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.del("n", "<leader>l")
+vim.keymap.del("n", "<leader>L")
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
--- .config/nvim/lua/plugins/leetcode.lua
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -1,6 +1,8 @@
+local leet_arg = "leetcode.nvim"
 return {
   {
     "kawre/leetcode.nvim",
+    lazy = leet_arg ~= vim.fn.argv()[1],
     build = ":TSUpdate html",
     dependencies = {
       "nvim-telescope/telescope.nvim",
@@ -12,9 +14,33 @@ return {
       "rcarriga/nvim-notify",
       "nvim-tree/nvim-web-devicons",
     },
+    keys = {
+      { "<leader>ll", "<cmd>Leet list<cr>", desc = "Leet list" },
+      { "<leader>lr", "<cmd>Leet run<cr>", desc = "Leet run" },
+      { "<leader>lt", "<cmd>Leet tabs<cr>", desc = "Leet tabs" },
+      { "<leader>lc", "<cmd>Leet console<cr>", desc = "Leet console" },
+      { "<leader>ls", "<cmd>Leet submit<cr>", desc = "Leet submit" },
+      { "<leader>lo", "<cmd>Leet open<cr>", desc = "Leet open" },
+    },
     opts = {
+      arg = leet_arg,
       --@type lc.lang
       lang = "golang",
+      -- which-key
+      function()
+        local wk = require("which-key")
+        wk.register({
+          ["<leader>l"] = {
+            name = "Leet",
+            l = { "Leet list" },
+            r = { "Leet run" },
+            t = { "Leet tabs" },
+            c = { "Leet console" },
+            s = { "Leet submit" },
+            o = { "Leet open" },
+          },
+        })
+      end,
     },
   },
 }

8e9aa9f5ddfa77a43a67ed610cfddaae174ef7ce
nanana37 <3na7nanana@gmail.com>
2024-04-24 13:22:21 +0900

    Update config for oil.nvim

--- .config/nvim/lua/plugins/oil-nvim.lua
+++ .config/nvim/lua/plugins/oil-nvim.lua
@@ -2,7 +2,34 @@ return {
   {
     "stevearc/oil.nvim",
     lazy = false,
-    opts = {},
+    keys = {
+      { "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" } },
+      -- Floating window does not support preview.
+      -- [Issue](https://github.com/stevearc/oil.nvim/issues/303) already exisits.
+      -- { "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" } },
+    },
+    opts = {
+      keymaps = {
+        ["-"] = "actions.close",
+        ["q"] = "actions.close",
+        ["<BS>"] = "actions.parent",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+        ["`"] = false,
+        ["~"] = false,
+        ["<C-g>"] = "actions.cd",
+        ["<C-t>"] = "actions.tcd",
+      },
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name)
+          return vim.startswith(name, ".git")
+        end,
+      },
+      float = {
+        padding = 5,
+      },
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },

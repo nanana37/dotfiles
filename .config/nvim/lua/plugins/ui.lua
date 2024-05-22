b89ae1bec12a761a1af17c0b1b035ecf33f0549c
nanana37 <3na7nanana@gmail.com>
2024-05-22 17:45:31 +0900

    Add zen-mode

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    keys = function()
+      return {
+        { "<leader>zz", ":ZenMode<CR>" },
+      }
+    end,
+    opts = {
+      plugins = { tmux = true },
+    },
+  },
+  {
+    "folke/twilight.nvim",
+  },
+}

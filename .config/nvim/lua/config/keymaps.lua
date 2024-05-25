28ae6dbe6459978149f17a423b142ef736c52f26
nanana37 <3na7nanana@gmail.com>
2024-05-26 01:07:36 +0900

    [nvim] keymaps for resize window

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,13 @@
 vim.keymap.del("n", "<leader>l")
 vim.keymap.del("n", "<leader>L")
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- Resize Window Height & Width
+vim.keymap.del("n", "<C-Up>")
+vim.keymap.del("n", "<C-Down>")
+vim.keymap.del("n", "<C-Left>")
+vim.keymap.del("n", "<C-Right>")
+vim.keymap.set("n", "<A-Up>", "<cmd>res +5<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<A-Down>", "<cmd>res -5<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<A-Right>", "<cmd>vert res +5<cr>", { desc = "Increase Window Width" })
+vim.keymap.set("n", "<A-Left>", "<cmd>vert res -5<cr>", { desc = "Decrease Window Width" })

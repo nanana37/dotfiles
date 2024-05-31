ceb6e8faa407cac89567f210a007c32c786afed6
nanana37 <hrk@macos>
2024-05-31 13:38:44 +0900

    [nvim] Obsidian: follow URL

--- packages/nvim/.config/nvim/lua/plugins/tools.lua
+++ packages/nvim/.config/nvim/lua/plugins/tools.lua
@@ -102,6 +102,12 @@ return {
         folder = "daily",
       },
       new_notes_location = "notes_subdir",
+      ---@param url string
+      follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        vim.fn.jobstart({ "open", url }) -- Mac OS
+        -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      end,
     },
   },
 }

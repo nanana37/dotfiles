7683b961ecf7215e734caf70c75e2e5b174c5977
nanana37 <3na7nanana@gmail.com>
2024-12-24 17:06:07 +0900

    [nvim] no need to toggle key for copilot

--- packages/nvim/.config/nvim/lua/plugins/copilot.lua
+++ packages/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,34 +1,34 @@
 return {
   { "zbirenbaum/copilot.lua", opts = { filetypes = { markdown = false } } },
-  {
-    "zbirenbaum/copilot.lua",
-    keys = {
-      {
-        "<leader>cps",
-        require("copilot.command").status,
-        mode = "n",
-        desc = "Copilot Status",
-      },
-      {
-        "<leader>cpe",
-        function()
-          local command = require("copilot.command")
-          command.enable()
-          command.status()
-        end,
-        mode = "n",
-        desc = "Enable Copilot",
-      },
-      {
-        "<leader>cpd",
-        function()
-          local command = require("copilot.command")
-          command.disable()
-          command.status()
-        end,
-        mode = "n",
-        desc = "Disable Copilot",
-      },
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   keys = {
+  --     {
+  --       "<leader>cps",
+  --       require("copilot.command").status,
+  --       mode = "n",
+  --       desc = "Copilot Status",
+  --     },
+  --     {
+  --       "<leader>cpe",
+  --       function()
+  --         local command = require("copilot.command")
+  --         command.enable()
+  --         command.status()
+  --       end,
+  --       mode = "n",
+  --       desc = "Enable Copilot",
+  --     },
+  --     {
+  --       "<leader>cpd",
+  --       function()
+  --         local command = require("copilot.command")
+  --         command.disable()
+  --         command.status()
+  --       end,
+  --       mode = "n",
+  --       desc = "Disable Copilot",
+  --     },
+  --   },
+  -- },
 }
